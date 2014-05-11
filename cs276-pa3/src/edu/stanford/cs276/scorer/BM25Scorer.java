403a8c6dae729a571b46c37fc0c02bf31fac03a1
Kavin Yao <kavinyao@gmail.com>
2014-05-10 21:38:45 -0700

    compute lengths of fields in BM25

--- cs276-pa3/src/edu/stanford/cs276/scorer/BM25Scorer.java
+++ cs276-pa3/src/edu/stanford/cs276/scorer/BM25Scorer.java
@@ -5,58 +5,98 @@ import edu.stanford.cs276.IDF;
 import edu.stanford.cs276.Query;
 import edu.stanford.cs276.doc.DocField;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
-public class BM25Scorer extends AScorer
-{
-    Map<Query,Map<String, Document>> queryDict;
+import static edu.stanford.cs276.doc.FieldProcessor.*;
 
-    public BM25Scorer(IDF idfs,Map<Query,Map<String, Document>> queryDict)
-    {
-        super(idfs);
-        this.queryDict = queryDict;
-        this.calcAverageLengths();
+public class BM25Scorer extends AScorer {
+    // static variables (parameters)
+    private static Map<DocField, Double> Bf;
+    private static Map<DocField, Double> Wf;
+    private double K1 = -1;
+    private double lambda = -1;
+    private double lambdaPrime = -1;
+
+
+    // initialize weights
+    static {
+        Bf = new HashMap<>();
+        Bf.put(DocField.url, 1.0);
+        Bf.put(DocField.title, 1.0);
+        Bf.put(DocField.header, 1.0);
+        Bf.put(DocField.body, 1.0);
+        Bf.put(DocField.anchor, 1.0);
+
+        Wf = new HashMap<>();
+        Wf.put(DocField.url, 1.0);
+        Wf.put(DocField.title, 1.0);
+        Wf.put(DocField.header, 1.0);
+        Wf.put(DocField.body, 1.0);
+        Wf.put(DocField.anchor, 1.0);
     }
 
+    // instance variables
+    // query -> url -> document
+    private Map<Query, Map<String, Document>> queryDict;
+    // field -> document -> length
+    Map<DocField, Map<Document, Double>> lengths;
+    // field -> avg. length
+    Map<DocField, Double> avgLengths;
+    // document -> pagerank
+    Map<Document, Double> pagerankScores;
 
-    ///////////////weights///////////////////////////
-    double urlweight = -1;
-    double titleweight  = -1;
-    double bodyweight = -1;
-    double headerweight = -1;
-    double anchorweight = -1;
-    
-    ///////bm25 specific weights///////////////
-    double burl=-1;
-    double btitle=-1;
-    double bheader=-1;
-    double bbody=-1;
-    double banchor=-1;
+    public BM25Scorer(IDF idfs, Map<Query, Map<String, Document>> queryDict) {
+        super(idfs);
 
-    double k1=-1;
-    double pageRankLambda=-1;
-    double pageRankLambdaPrime=-1;
-    //////////////////////////////////////////
-    
-    ////////////bm25 data structures--feel free to modify ////////
-    
-    Map<Document,Map<String,Double>> lengths;
-    Map<String,Double> avgLengths;
-    Map<Document,Double> pagerankScores;
-    
-    //////////////////////////////////////////
-    
-    //sets up average lengths for bm25, also handles pagerank
-    public void calcAverageLengths()
-    {
-        lengths = new HashMap<Document,Map<String,Double>>();
-        avgLengths = new HashMap<String,Double>();
-        pagerankScores = new HashMap<Document,Double>();
+        this.queryDict = queryDict;
 
-        /*
-         * @//TODO : Your code here
-         */
+        calcAverageLengths();
+    }
+
+    private static Map<Document, Double> lengthsOfField(Collection<Map<String, Document>> docs,
+                                                 Function<Document, Integer> getLength) {
+        return docs
+                .stream()
+                .map(m -> m.values())
+                .flatMap(c -> c.stream())
+                .collect(Collectors.toMap(d -> d, d -> getLength.apply(d).doubleValue()));
+    }
+
+    private static Double averageFieldLength(Map<Document, Double> fieldLengths) {
+        return fieldLengths.values()
+                .stream()
+                .mapToDouble(d -> d)
+                .average()
+                .getAsDouble();
+    }
+
+    // sets up average lengths for bm25, also handles pagerank
+    public void calcAverageLengths() {
+        Collection<Map<String, Document>> docs = queryDict.values();
+
+        // compute length of each field
+        lengths = new HashMap<>();
+        lengths.put(DocField.url, lengthsOfField(docs, d -> splitURL(d).size()));
+        lengths.put(DocField.title, lengthsOfField(docs, d -> splitTitle(d).size()));
+        lengths.put(DocField.header, lengthsOfField(docs, d -> splitHeaders(d).size()));
+        lengths.put(DocField.body, lengthsOfField(docs, d -> d.bodyLength));
+        lengths.put(DocField.anchor, lengthsOfField(docs, d -> splitAnchors(d).size()));
+
+        // compute average lengths of each field
+        avgLengths = new HashMap<>();
+        for (DocField docField : DocField.values()) {
+            avgLengths.put(docField, averageFieldLength(lengths.get(docField)));
+        }
+
+        pagerankScores = docs
+                .stream()
+                .map(m -> m.values())
+                .flatMap(c -> c.stream())
+                .collect(Collectors.toMap(d -> d, d -> new Double(d.pageRank)));
 
         //normalize avgLengths
         //for (String tfType : this.TF_TYPES)
@@ -67,12 +107,11 @@ public class BM25Scorer extends AScorer
         //}
 
     }
-    
+
     ////////////////////////////////////
-    
-    
-    public double getNetScore(Map<DocField, Map<String, Double>> tfs, Query q, Map<String,Double> tfQuery,Document d)
-    {
+
+
+    public double getNetScore(Map<DocField, Map<String, Double>> tfs, Query q, Map<String, Double> tfQuery, Document d) {
         double score = 0.0;
 
         /*
@@ -83,8 +122,7 @@ public class BM25Scorer extends AScorer
     }
 
     //do bm25 normalization
-    public void normalizeTFs(Map<DocField, Map<String, Double>> tfs,Document d, Query q)
-    {
+    public void normalizeTFs(Map<DocField, Map<String, Double>> tfs, Document d, Query q) {
         /*
          * @//TODO : Your code here
          */
@@ -92,20 +130,17 @@ public class BM25Scorer extends AScorer
 
 
     @Override
-    public double getSimScore(Document d, Query q)
-    {
+    public double getSimScore(Document d, Query q) {
 
-        Map<DocField,Map<String, Double>> tfs = this.getDocTermFreqs(d,q);
+        Map<DocField, Map<String, Double>> tfs = this.getDocTermFreqs(d, q);
 
         this.normalizeTFs(tfs, d, q);
 
-        Map<String,Double> tfQuery = getQueryFreqs(q);
+        Map<String, Double> tfQuery = getQueryFreqs(q);
 
 
-        return getNetScore(tfs,q,tfQuery,d);
+        return getNetScore(tfs, q, tfQuery, d);
     }
-
-
 
 
 }
