810e97f0c2e88dd6eec6fc1ec134b8c59f2c1bdf
Kavin Yao <kavinyao@gmail.com>
2014-05-11 22:25:02 -0700

    implement dumb smallest window scorer based on cosine scorer

--- cs276-pa3/src/edu/stanford/cs276/scorer/SmallestWindowScorer.java
+++ cs276-pa3/src/edu/stanford/cs276/scorer/SmallestWindowScorer.java
@@ -3,51 +3,20 @@ package edu.stanford.cs276.scorer;
 import edu.stanford.cs276.Document;
 import edu.stanford.cs276.IDF;
 import edu.stanford.cs276.Query;
-import edu.stanford.cs276.doc.DocField;
 
-import java.util.Map;
+public class SmallestWindowScorer extends CosineSimilarityScorer {
+    double B = -1;
 
-//doesn't necessarily have to use task 2 (could use task 1, in which case, you'd probably like to extend CosineSimilarityScorer instead)
-public class SmallestWindowScorer extends BM25Scorer
-{
-
-    /////smallest window specifichyperparameters////////
-    double B = -1;    	    
-    double boostmod = -1;
-    
-    //////////////////////////////
-
-    public SmallestWindowScorer(IDF idfs,Map<Query,Map<String, Document>> queryDict)
-    {
-        super(idfs, queryDict);
-        handleSmallestWindow();
+    public SmallestWindowScorer(IDF idfs) {
+        super(idfs);
     }
-
-
-    public void handleSmallestWindow()
-    {
-        /*
-         * @//TODO : Your code here
-         */
-    }
-
-
-    public double checkWindow(Query q,String docstr,double curSmallestWindow,boolean isBodyField)
-    {
-        /*
-         * @//TODO : Your code here
-         */
-        return -1;
-    }
-
 
     @Override
     public double getSimScore(Document d, Query q) {
-        Map<DocField,Map<String, Double>> tfs = this.getDocTermFreqs(d,q);
-
-        Map<String,Double> tfQuery = getQueryFreqs(q);
-
-        return 0;
+        return super.getSimScore(d, q) * getBoost(d, q);
     }
 
+    private double getBoost(Document d, Query q) {
+        return 1;
+    }
 }
